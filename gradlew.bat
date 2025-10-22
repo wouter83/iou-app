3b4da00f1b528628f47b71a7514a7655f5a7f540
Wouter Kok <wouterkok@gmail.com>
2025-10-22 20:34:53 +0200

    Track Gradle wrapper JAR; remove base64 and decode fallbacks; keep standard wrapper

--- gradlew.bat
+++ gradlew.bat
@@ -1,66 +1,94 @@
-@ECHO OFF
-SETLOCAL
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+@rem SPDX-License-Identifier: Apache-2.0
+@rem
 
-SET APP_BASE_NAME=%~n0
-SET APP_HOME=%~dp0
+@if "%DEBUG%"=="" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
 
-SET DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
 
-IF NOT "%JAVA_HOME%"=="" GOTO findJavaFromJavaHome
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+@rem This is normally unused
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
 
-SET JAVA_EXE=java.exe
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-IF "%ERRORLEVEL%" == "0" GOTO init
+if %ERRORLEVEL% equ 0 goto execute
 
-ECHO.
-ECHO ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-GOTO fail
+echo. 1>&2
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
+echo. 1>&2
+echo Please set the JAVA_HOME variable in your environment to match the 1>&2
+echo location of your Java installation. 1>&2
+
+goto fail
 
 :findJavaFromJavaHome
-SET JAVA_HOME=%JAVA_HOME:"=%
-SET JAVA_EXE=%JAVA_HOME%\bin\java.exe
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-IF EXIST "%JAVA_EXE%" GOTO init
+if exist "%JAVA_EXE%" goto execute
 
-ECHO.
-ECHO ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-ECHO Please set the JAVA_HOME variable in your environment to match the
-ECHO location of your Java installation.
+echo. 1>&2
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
+echo. 1>&2
+echo Please set the JAVA_HOME variable in your environment to match the 1>&2
+echo location of your Java installation. 1>&2
 
-:fail
-PAUSE
-EXIT /B 1
-
-:init
-SET WRAPPER_JAR=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-SET WRAPPER_B64=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar.base64
-
-IF NOT EXIST "%WRAPPER_JAR%" (
-    IF EXIST "%WRAPPER_B64%" (
-        powershell -NoProfile -ExecutionPolicy Bypass -Command "\$b64Path = [System.IO.Path]::GetFullPath('%WRAPPER_B64%'); \$jarPath = [System.IO.Path]::GetFullPath('%WRAPPER_JAR%'); \$bytes = [System.Convert]::FromBase64String((Get-Content -Raw -Path \$b64Path)); [System.IO.File]::WriteAllBytes(\$jarPath, \$bytes)" >NUL
-        IF NOT EXIST "%WRAPPER_JAR%" (
-            ECHO.
-            ECHO ERROR: Failed to decode Gradle wrapper JAR.
-            GOTO fail
-        )
-    ) ELSE (
-        ECHO.
-        ECHO ERROR: Gradle wrapper JAR is missing. Run "gradle wrapper" to regenerate it.
-        GOTO fail
-    )
-)
-
-SET CLASSPATH=%WRAPPER_JAR%
-
-SET CMD_LINE_ARGS=
-:parse
-IF "%1"=="" GOTO execute
-SET CMD_LINE_ARGS=%CMD_LINE_ARGS% "%1"
-SHIFT
-GOTO parse
+goto fail
 
 :execute
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+@rem Setup the command line
 
-ENDLOCAL
-EXIT /B 0
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
+@rem End local scope for the variables with windows NT shell
+if %ERRORLEVEL% equ 0 goto mainEnd
+
+:fail
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
+set EXIT_CODE=%ERRORLEVEL%
+if %EXIT_CODE% equ 0 set EXIT_CODE=1
+if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
+exit /b %EXIT_CODE%
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+:omega
